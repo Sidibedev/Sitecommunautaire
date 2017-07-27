efa08f8066d409bc71a728669b77bbf06843add6
mouhamed <mouhamed.aly.sidibe96@gmail.com>
2017-07-27 14:30:24 -0100

    CRUD DONE

--- acceuil.py
+++ acceuil.py
@@ -2,13 +2,15 @@
 #coding=utf-8
 from flask import Flask  
 from flask_pymongo import PyMongo
-from flask import render_template , request
+from flask import render_template , request ,redirect
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'test'
 
 mongo = PyMongo(app);
+
 
 
 
@@ -32,11 +34,50 @@ def add():
    	email = request.form['email']
    	titre = request.form['titre']
    	article.insert_one({'titre' : titre , 'auteur' : { 'nom' : nom , 'prenom' : prenom , 'email' : email} })
-    
-
-   	
 
    return render_template('index.html' , articles=articles)	
+    
+
+@app.route('/delete')
+def delete():
+    article = mongo.db.articles
+    key=request.values.get("id")
+    article.remove({"_id":ObjectId(key)})
+    return redirect("/")
+	
+@app.route('/update')
+def update():
+    article = mongo.db.articles
+    id=request.values.get("idmodif")
+    articleToUpdate = mongo.db.articles.find({"_id":ObjectId(id)})
+    return render_template('modif.html', articleToUpdate=articleToUpdate)
+    
+@app.route('/updatearticle' , methods=['POST'])
+def updatearticle():
+    prenom = request.form['prenom']
+    nom = request.form['nom']
+    email = request.form['email']
+    titre = request.form['titre']
+    idarticle = request.form['id']
+    article = mongo.db.articles
+    article.update({"_id":ObjectId(idarticle)}, {'$set':{ "titre":titre, "auteur":{ 'nom' : nom , 'prenom' : prenom , 'email' : email } }})
+    return redirect("/")
+
+
+
+@app.route('/search' , methods=['POST'])
+def search():
+    search = request.form['search']
+    ref = request.form['ref']
+    articlesearch =  mongo.db.articles.find({ref : search})
+    return render_template('search.html' , articlesearch=articlesearch)
+
+
+
+
+
+
+   
 if __name__ == "__main__":
             app.run()
 
